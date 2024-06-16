6e6c10e2ac2774c544089f0eaf2f3b5c1783dd55
Navdeep <21bcs037>
2024-06-16 20:01:31 +0530

    created new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile'
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+      <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+      <Route path="/about" element={<About></About>}></Route> 
+      <Route path="/profile" element={<Profile></Profile>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
