f3860891f3c6c6c3ad76740c6eb273cfaef69468
Navdeep <21bcs037>
2024-06-22 23:08:23 +0530

    added files

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://navik81254:Barmana@cluster0.mwp78za.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 const app=express();
 
